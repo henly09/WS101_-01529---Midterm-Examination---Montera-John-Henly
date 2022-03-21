79fb3a0e857b43bda01fe42a06350148bf9942f0
John Henly A. Montera <monterahens@gmail.com>
2022-03-21 16:54:43 +0800

    initial commit

--- app/Http/Controllers/atmcontroller.php
+++ app/Http/Controllers/atmcontroller.php
@@ -7,11 +7,10 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use DB;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\View;
 
 class atmcontroller extends Controller
 {
-
-    protected $uniname = "henz";
 
     public function login(){
         return view('login');
@@ -42,7 +41,9 @@ class atmcontroller extends Controller
      $user = DB::table('atmuser')->where('username', '=', $username)->where('password', '=', $password)->get();
      $usercount = $user->count();
      $name = DB::table('atmuser')->where('username', '=' ,$username)->where('password', '=', $password)->pluck('name');
-     $this->uniname = DB::table('atmuser')->where('username', '=' ,$username)->where('password', '=', $password)->pluck('name');
+
+     $request->session()->put('uniname', $name);
+     
      $balance = DB::table('atmuser')->where('username', '=' ,$username)->where('password', '=', $password)->pluck('balance');
 
         if (!$usercount == 0) {
@@ -82,15 +83,17 @@ class atmcontroller extends Controller
             'withdraw' => 'required'
         ]);
 
+        $value = session('uniname');
+
         $withdraw = (int)$request->input('withdraw');
         $type = 'Withdraw';
 
-        $balance = DB::table('atmuser')->where('name', '=' ,$this->uniname)->value('balance');
-        $username = DB::table('atmuser')->where('name', '=' ,$this->uniname)->pluck('username');
+        $balance = DB::table('atmuser')->where('name', '=' ,$value)->value('balance');
+        $username = DB::table('atmuser')->where('name', '=' ,$value)->pluck('username');
 
             $newbalance =  $balance - $withdraw;
-            DB::table('atmuser')->where('name', $this->uniname)->update(array('balance' => $newbalance));
-            $data=array('name'=>$this->uniname,"username"=>$username,"type"=>$type,"transacmoney"=>$withdraw,"created_at" =>  \Carbon\Carbon::now(),"updated_at" => \Carbon\Carbon::now());
+            DB::table('atmuser')->where('name', $value)->update(array('balance' => $newbalance));
+            $data=array('name'=>$value,"username"=>$username,"type"=>$type,"transacmoney"=>$withdraw,"created_at" =>  \Carbon\Carbon::now(),"updated_at" => \Carbon\Carbon::now());
             DB::table('history')->insert($data);
 
             return view('login');
@@ -102,15 +105,17 @@ class atmcontroller extends Controller
             'deposit' => 'required'
         ]);
 
+        $value = session('uniname');
+
         $deposit = (int)$request->input('deposit');
         $type = 'Deposit';
 
-        $balance = DB::table('atmuser')->where('name', '=' ,$this->uniname)->value('balance');
-        $username = DB::table('atmuser')->where('name', '=' ,$this->uniname)->pluck('username');
+        $balance = DB::table('atmuser')->where('name', '=' ,$value)->value('balance');
+        $username = DB::table('atmuser')->where('name', '=' ,$value)->pluck('username');
 
             $newbalance =  $balance - $deposit;
-            DB::table('atmuser')->where('name', $this->uniname)->update(array('balance' => $newbalance));
-            $data=array('name'=>$this->uniname,"username"=>$username,"type"=>$type,"transacmoney"=>$deposit,"created_at" =>  \Carbon\Carbon::now(),"updated_at" => \Carbon\Carbon::now());
+            DB::table('atmuser')->where('name', $value)->update(array('balance' => $newbalance));
+            $data=array('name'=>$value,"username"=>$username,"type"=>$type,"transacmoney"=>$deposit,"created_at" =>  \Carbon\Carbon::now(),"updated_at" => \Carbon\Carbon::now());
             DB::table('history')->insert($data);
 
             return view('login');
